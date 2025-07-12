82f0648bc8648fb3e559a21ca6fc47ef588af031
legendkuria <kuriakevin06@gmail.com>
2025-07-12 20:19:44 +0300

    Added Registration API

--- fh_online/services/rest.py
+++ fh_online/services/rest.py
@@ -49,7 +49,7 @@ def get_default_currency():
         return {'error': str(e)}, 400
 
 
-@frappe.whitelist(allow_guest=True, methods=["GET"])
+@frappe.whitelist(allow_guest=True, methods="POST")
 def customer_registration(**kwargs):
     try:
         mobile_no = kwargs.get('mobile_number')
@@ -223,7 +223,7 @@ def send_opt(mobile_number):
         }
 
 
-@frappe.whitelist(allow_guest= True, methods="POST")
+@frappe.whitelist(allow_guest= True, methods="GET")
 def validate_otp_exists(**kwargs):
     try:
         mobile_number = kwargs.get('mobile_number')
@@ -238,7 +238,7 @@ def validate_otp_exists(**kwargs):
         return False
 
 
-@frappe.whitelist(allow_guest=True, methods="GET")
+@frappe.whitelist(allow_guest=True, methods="POST")
 def password_recovery(usr, new_password):
     try:
         if not usr or not new_password:
@@ -260,3 +260,56 @@ def password_recovery(usr, new_password):
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), f"Password Recovery Error: {str(e)}")
         return {'error': str(e)}, 500
+
+
+@frappe.whitelist( allow_guest=True )
+def login(usr, pwd):
+    try:
+        try:
+            login_manager = frappe.auth.LoginManager()
+            login_manager.authenticate(user=usr, pwd=pwd)
+            login_manager.post_login()
+        except frappe.exceptions.AuthenticationError:
+            frappe.clear_messages()
+            frappe.local.response["message"] = {
+                "success_key":0,
+                "message":"Authentication Error!"
+            }
+
+            return
+
+        api_generate = generate_keys(frappe.session.user)
+        user = frappe.get_doc('User', frappe.session.user)
+
+        frappe.response["message"] = {
+            "success_key":1,
+            "message":"Authentication success",
+            "sid":frappe.session.sid,
+            "api_key":user.api_key,
+            "api_secret":api_generate,
+            "username":user.username,
+            "email":user.email,
+            "base_url": frappe.utils.get_url()
+        }
+    
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), f"Error in login: {e}")
+        return {"status": "error", "message": str(e)}
+
+def generate_keys(user):
+    try:
+        user_details = frappe.get_doc('User', user)
+        api_secret = frappe.generate_hash(length=15)
+
+        if not user_details.api_key:
+            api_key = frappe.generate_hash(length=15)
+            user_details.api_key = api_key
+
+        user_details.api_secret = api_secret
+        user_details.save()
+
+        return api_secret
+    
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), f"Error in login: {e}")
+        return {"status": "error", "message": str(e)}
